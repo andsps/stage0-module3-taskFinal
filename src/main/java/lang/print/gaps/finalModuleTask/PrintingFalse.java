5dd8b4d9c3fdd9b905bbfb6e67b0ea00d715dd00
andsps <spirkevichusandrei@mail.ru>
2024-01-11 08:58:21 +0300

    2 answers

--- src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
+++ src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
@@ -4,6 +4,8 @@ public class PrintingFalse {
     public static void main(String[] args) {
         int first = 9;
         int second = -17;
+        boolean myFirst = first < second;
+        System.out.println(myFirst);
         //write code here
     }
 }

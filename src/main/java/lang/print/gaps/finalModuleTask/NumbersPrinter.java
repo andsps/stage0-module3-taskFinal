32b09a8dd440b9146fe97cc8fe2a899c55d22528
andsps <spirkevichusandrei@mail.ru>
2024-01-11 11:43:24 +0300

    2 answers

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
+        boolean isPositive = number > 0;
+        System.out.println(true);
 
+        boolean isNegative = number < 0;
+        System.out.println(false);
     }
 }
